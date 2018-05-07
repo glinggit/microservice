4beb74e7185fd0582cd168f56433fa7fefe7f867
syygl <so2ycit@163.com>
2018-05-07 17:33:37 +0800

    new project

---
+++ eureka-zipkin/src/main/java/com/example/demo/EurekaZipkinApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaZipkinApplication.class, args);
+	}
+}
