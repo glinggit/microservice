49a620b9dc30be6f55c29837c79398aee5bea54d
syygl <so2ycit@163.com>
2018-05-15 10:51:37 +0800

    hsytrix-turbine

--- new-eureka-client/src/main/java/com/example/demo/NewEurekaClientApplication.java
+++ new-eureka-client/src/main/java/com/example/demo/NewEurekaClientApplication.java
@@ -4,26 +4,36 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class NewEurekaClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewEurekaClientApplication.class, args);
 	}
-	
+
 	@Value("${server.port}")
-    String port;
-	
-	
+	String port;
+
 	@RequestMapping("/hi")
-    public String home(@RequestParam String name) {
-        return "hi "+name+",i am from port:" +port;
-    }
-	
+	@HystrixCommand(fallbackMethod = "hiError")
+	public String home(@RequestParam String name) {
+		return "hi " + name + ",i am from port:" + port;
+	}
+
+	public String hiError(String name) {
+		return "hi," + name + ",sorry,error!";
+	}
+
 }
